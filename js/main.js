43693baadc8d4aabbbd27f5679a3880a1e4a9cb2
Avi Parshan <avi@aviparshan.com>
2020-06-16 22:16:48 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var locations = ["Tel Aviv", "Jerusalem", "Modiin", "Eilat"];
+
+function myFunction() {
+    var x, text;
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("pass").value;
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x <= 0) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
+
+$(document).ready(function () {
+    $('.clockpicker').clockpicker();
+
+    $('#calc').click(function () {
+        alert("Calc");
+    });
+});
